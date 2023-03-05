3a0cb16d75d245cfbf231b221aa17e5a4a917f12
boobaGreen <devboobagreen@gmail.com>
2023-03-05 22:54:25 +0100

    accenno_bottoni

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,11 @@ let Border_Top;
 let Border_Left;
 let Small_H;
 let Small_W;
-let canvasSheep = document.getElementById("canvasSheep");
-const ctx = document.getElementById("canvasSheep").getContext("2d");
+let newBtna;
+let newBtnb;
+let newBtnc;
+let primoGiro = true;
+
 const img = new Image(); // Create new img element
 img.src = "/assets/img/pecorelle_t.png";
 let CanvasWidth;
@@ -24,15 +27,13 @@ function sleep(ms) {
 }
 
 function drawSheep() {
+  let canvasSheep = document.getElementById("canvasSheep");
+  const ctx = document.getElementById("canvasSheep").getContext("2d");
   const img = new Image();
   const offSetRiga = 0;
   const offSetColonna = PositionSheepFrameSheetSelect[actualFrame];
   img.onload = () => {
-    console.log(CanvasHeight);
-    console.log(CanvasWidth);
-
     ctx.clearRect(0, 0, canvasSheep.width, canvasSheep.height);
-
     canvasSheep.style.position = "absolute";
     canvasSheep.style.bottom = SheepPositionActualBottom + "px";
     canvasSheep.style.left = SheepPositionActualLeft + "px";
@@ -50,12 +51,45 @@ function drawSheep() {
     );
   };
   img.src = "/assets/img/pecorelle_t.png";
-  actualFrame++;
-}
 
+  if (actualFrame > 4) {
+    SheepPositionActualBottom = 20; // da mettre calcolo
+  }
+}
+function drawBtn() {
+  // let btn = document.createElement("button");
+  // btn.innerText = "Inc";
+  // document.body.appendChild(btn);
+
+  let fs = Math.round((W / 250) * 10) / 10;
+  if (primoGiro) {
+    newBtna = document.createElement("button");
+    newBtna.id = "btnDec";
+    newBtna.innerText = "-";
+    newBtna.className = "btnact";
+    document.querySelector("#dec").appendChild(newBtna);
+    newBtnb = document.createElement("button");
+    newBtnb.id = "btnRst";
+    newBtnb.innerText = "Reset";
+    newBtnb.className = "btnact";
+    document.querySelector("#rst").appendChild(newBtnb);
+    newBtnc = document.createElement("button");
+    newBtnc.className = "btnact";
+    newBtnc.id = "btnInc";
+    newBtnc.innerText = "+";
+    document.querySelector("#inc").appendChild(newBtnc);
+    primoGiro = false;
+  }
+
+  newBtna.style.fontSize = fs + "rem";
+
+  newBtnb.style.fontSize = fs + "rem";
+
+  newBtnc.style.fontSize = fs + "rem";
+}
 function windowResize() {
-  H = document.getElementById("design-area").clientHeight; // la dimensione senza barre page-up/dw
-  W = document.getElementById("design-area").clientWidth;
+  H = document.getElementById("designArea").clientHeight; // la dimensione senza barre page-up/dw
+  W = document.getElementById("designArea").clientWidth;
   console.log("Big_H :", H);
   console.log("Big_W :", W);
 
@@ -67,7 +101,6 @@ function windowResize() {
     Border_Top = Math.trunc((H - Small_H) / 2);
     Border_Left = 0;
   } else {
-    Bordi_Top = false;
     Border_Top = 0;
     Small_W = Math.trunc(RATIO_BKGR * H);
     Small_H = H;
@@ -77,10 +110,10 @@ function windowResize() {
   console.log("Small_W :", Small_W);
   console.log("Border-Top: ", Border_Top);
   console.log("Border-Left: ", Border_Left);
-  document.getElementById("paint-area").style.width = Small_W + "px";
-  document.getElementById("paint-area").style.height = Small_H + "px";
-  // div posizionao rimane un piccolo glitch ogni tanto con refresh passa tutto
-  var d = document.getElementById("paint-area");
+  document.getElementById("paintArea").style.width = Small_W + "px";
+  document.getElementById("paintArea").style.height = Small_H + "px";
+
+  var d = document.getElementById("paintArea");
   d.style.position = "absolute";
   d.style.left = Border_Left + "px";
   d.style.top = Border_Top + "px";
@@ -93,26 +126,28 @@ function windowResize() {
 
   console.log(CanvasHeight);
   console.log(CanvasWidth);
+  let fs = Math.round((W / 250) * 10) / 10;
   drawSheep();
+  drawBtn();
 }
 
 async function init() {
   window.addEventListener("resize", windowResize);
   windowResize();
 
-  for (let i = 1; i < 9; i++) {
-    await sleep(2000);
+  for (let i = 0; i < 8; i++) {
+    actualFrame++;
+    await sleep(500);
     if (actualFrame == 0) {
-      SheepPositionActualBottom = 300;
+      windowResize();
     } else {
+      if (actualFrame == 4) {
+        SheepPositionActualBottom = Small_H / 5;
+      }
       SheepPositionActualLeft = actualFrame * (Small_W / 10) + 20;
     }
-    //SheepPositionActualLeft = actualFrame * CanvasWidth + CanvasWidth;
-    if (actualFrame < 8) {
-    } else {
-    }
 
-    drawSheep();
+    windowResize();
   }
 }
 window.onload = init;
